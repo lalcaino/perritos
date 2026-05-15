5e44dd2c12b5611fdb0d0f8851fcefd2ad8ec042
Lucas Alcaino <l.alcaino@duocuc.cl>
2026-05-15 10:06:26 -0400

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     descripcion VARCHAR(255),
-    precio DECIMAL(10,2) NOT NULL,
+    causa DECIMAL(10,2) NOT NULL,
     stock INT NOT NULL
 );
 
